cc3f34d51a141dbe35472c8429b36875a459373a
Luuk van Dijk <Luukvandijk2003@gmail.com>
2022-03-06 14:51:55 +0100

    + button toegevoegd

--- GTS-golven/screen/Dashboard.js
+++ GTS-golven/screen/Dashboard.js
@@ -17,6 +17,14 @@ const Dashboard = props => {
                     snapToInterval={200} 
                     snapToAlignment={'center'}
                     >
+                    <View style={styles.container}>
+                        <View style={styles.roundConatiner}>
+                            <View style={styles.conatinerPlus}>
+                                <Text style={styles.plus}>+</Text>
+                            </View>
+                        </View>
+                        <Text style={styles.text}>Upload jouw video</Text>
+                    </View>
                     <Card title='Stroke 1 - i5' date='09-08-2022' />
                     <Card title='Stroke 2 - i5' date='09-08-2022' />
                     <Card title='Stroke 3 - i5' date='09-08-2022' />
@@ -31,6 +39,36 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: 50,
     },
+
+    container: {
+        width: 200,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    roundConatiner: {
+        backgroundColor: Colors.primary,
+        width: 130,
+        height: 130,
+        borderRadius: 100,
+    },
+
+    conatinerPlus: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%'
+    },
+
+    plus: {
+        fontSize: 50,
+    },
+
+    text: {
+        marginTop: 10,
+        fontSize: 20,
+    }
 });
 
 export default Dashboard;
