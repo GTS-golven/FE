21e31159f6ad58e3dab3123fab904194bca59557
Luuk van Dijk <luukvandijk2003@gmail.com>
2022-03-09 13:11:03 +0100

    Image upload werkt

--- GTS-golven/screen/Dashboard.js
+++ GTS-golven/screen/Dashboard.js
@@ -1,34 +1,85 @@
-import React from 'react'
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView, Pressable, Image, StyleSheet } from 'react-native'
 
 import Colors from '../components/Colors';
 import Card from '../components/Card'
 
+import * as ImagePicker from 'expo-image-picker';
+
 const Dashboard = props => {
+
+    const [pickedImagePath, setPickedImagePath] = useState('');
+
+    const showImagePicker = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("You've refused to allow this appp to access your photos!");
+            return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync();
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setPickedImagePath(result.uri);
+            console.log(result.uri);
+        }
+    }
+
+    const openCamera = async () => {
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("You've refused to allow this appp to access your camera!");
+            return;
+        }
+
+        const result = await ImagePicker.launchCameraAsync();
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setPickedImagePath(result.uri);
+            console.log(result.uri);
+        }
+    }
+
     return (
         <View style={styles.screen}>
             <View style={styles.cards}>
                 <ScrollView
-                    horizontal={true} 
-                    alwaysBounceHorizontal={true} 
-                    pagingEnabled={true} 
-                    showsHorizontalScrollIndicator={false} 
-                    decelerationRate={'fast'} 
-                    snapToInterval={200} 
+                    horizontal={true}
+                    alwaysBounceHorizontal={true}
+                    pagingEnabled={true}
+                    showsHorizontalScrollIndicator={false}
+                    decelerationRate={'fast'}
+                    snapToInterval={200}
                     snapToAlignment={'center'}
-                    >
-                    <View style={styles.container}>
-                        <View style={styles.roundConatiner}>
-                            <View style={styles.conatinerPlus}>
-                                <Text style={styles.plus}>+</Text>
+                >
+                    <Pressable onPress={showImagePicker}>
+                        <View style={styles.container}>
+                            <View style={styles.roundConatiner}>
+                                <View style={styles.conatinerPlus}>
+                                    <Text style={styles.plus}>+</Text>
+                                </View>
                             </View>
+                            <Text style={styles.text}>Upload jouw video</Text>
                         </View>
-                        <Text style={styles.text}>Upload jouw video</Text>
-                    </View>
+                    </Pressable>
                     <Card title='Stroke 1 - i5' date='09-08-2022' />
                     <Card title='Stroke 2 - i5' date='09-08-2022' />
                     <Card title='Stroke 3 - i5' date='09-08-2022' />
                 </ScrollView>
+            </View>
+            <View>
+                {
+                    pickedImagePath !== '' && <Image
+                        source={{ uri: pickedImagePath }}
+                        style={styles.image}
+                    />
+                }
             </View>
         </View>
     )
@@ -68,6 +119,12 @@ const styles = StyleSheet.create({
     text: {
         marginTop: 10,
         fontSize: 20,
+    },
+
+    image: {
+        width: 400,
+        height: 300,
+        resizeMode: 'cover',
     }
 });
 
