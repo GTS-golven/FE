0bc0ab646b31569bd616bfc63c732180bd558bf4
Luuk van Dijk <Luukvandijk2003@gmail.com>
2022-03-10 12:48:09 +0100

    Info over video

--- GTS-golven/App.js
+++ GTS-golven/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from './components/Colors';
+
+import Gallery from './screen/Gallery'
 
 export default function App() {
   return (
     <View style={styles.screen}>
-
+      <Gallery />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+    backgroundColor: Colors.backgournd,
   },
 });
