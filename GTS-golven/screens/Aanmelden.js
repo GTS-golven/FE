4d919a49c5024a12a7210245baed532eb3fe9564
Luuk van Dijk <luukvandijk2003@gmail.com>
2022-04-11 12:49:25 +0200

    aanmelden werkt

--- GTS-golven/screens/Aanmelden.js
+++ GTS-golven/screens/Aanmelden.js
@@ -9,26 +9,50 @@ import {
 } from "react-native";
 
 import Colors from "../components/Colors";
-// import UserService from "../services/UserService";
-// var userService = new UserService();
-// var email = "Test mail";
-// var pass = "Test wachtwoord";
-// var test123;
-// test123.email = email;
-// test123.password = pass;
-// var test = userService.post(test123);
+
+import UserService from "../services/UserService";
+var userService = new UserService();
 
 const Dashboard = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
+  const [checkEmail, setCheckEmail] = useState(false);
+
+  const validate = (email) => {
+    setEmail(email);
+    const expression =
+      /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+    if (expression.test(String(email).toLowerCase())) {
+      setCheckEmail(true);
+    }
+
+    return expression.test(String(email).toLowerCase());
+  };
+
+  async function Register() {
+    var dict = {
+      username: email,
+      password: password,
+    };
+
+    console.log(dict);
+
+    // var test = await userService.post(dict);
+    // console.log(test);
+  }
 
   const passCheck = () => {
-    if (checkPassword === password) {
-      navigation.push("ConfirmMail");
+    if (checkPassword === password && checkEmail === true) {
+      Register();
+      navigation.push("Dashboard");
     }
     if (checkPassword !== password) {
-      console.log("Wachwoorden zijn niet gelijk");
+      console.warn("Wachwoorden komen niet over een");
+    }
+    if (checkEmail !== true) {
+      console.warn("Voer een geldig email adress in");
     }
   };
 
@@ -43,8 +67,7 @@ const Dashboard = ({ navigation }) => {
               style={styles.textinpput}
               autoComplete="email"
               keyboardAppearance="email-address"
-              onChangeText={setEmail}
-              value={email}
+              onChangeText={validate}
             />
           </View>
           <View style={styles.inputContainer}>
--- GTS-golven/services/UserService.ts
+++ GTS-golven/services/UserService.ts
@@ -2,7 +2,7 @@ import FetchService from "./FetchService";
 
 class UserService extends FetchService {
   constructor() {
-    super("auth/user/");
+    super("auth/register/");
   }
 }
 
