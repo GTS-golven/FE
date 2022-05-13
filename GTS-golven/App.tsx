f3e87f9e2af4dcc47e50d9579fe6f5aaac617015
Luuk van Dijk <Luukvandijk2003@gmail.com>
2022-05-13 19:13:49 +0200

    animatie tussen schremen

--- GTS-golven/App.tsx
+++ GTS-golven/App.tsx
@@ -36,95 +36,95 @@ function App() {
       <NavigationContainer>
         <AuthStack.Navigator>
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Start"
             component={Start}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Login"
             component={Login}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="WachtwoordVergeten"
             component={WachtwoordVergeten}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="VerranderWachtwoord"
             component={VerranderWachtwoord}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Aanmelden"
             component={Aanmelden}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="ConfirmMail"
             component={ConfirmMail}
           />
 
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "none" }}
             name="Dashboard"
             component={Dashboard}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Load"
             component={Load}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="AdInfo"
             component={AdInfo}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Gallery"
             component={Gallery}
           />
 
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "none" }}
             name="Profiel"
             component={Profiel}
           />
 
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "none" }}
             name="Settings"
             component={Settings}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Vragen"
             component={Vragen}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="MaakVideo"
             component={MaakVideo}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="WatIsData"
             component={WatIsData}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="AccuraatData"
             component={AccuraatData}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="Contact"
             component={Contact}
           />
           <AuthStack.Screen
-            options={{ headerShown: false }}
+            options={{ headerShown: false, animation: "fade" }}
             name="NewPassword"
             component={NewPassword}
           />
