60bf51df3d1ba9dfd3e52710ad8c522f8b1d746e
Luuk van Dijk <luukvandijk2003@gmail.com>
2022-06-02 14:05:36 +0200

    add offline to dashboard

--- GTS-golven/screens/AppScreens/Dashboard.js
+++ GTS-golven/screens/AppScreens/Dashboard.js
@@ -16,19 +16,22 @@ import Popup from "../../components/Popup";
 import Gallery from "../../components/Gallery";
 import axios from "axios";
 import simulatie from "../../assets/simulation.png";
+import { getNativeSourceAndFullInitialStatusForLoadAsync } from "expo-av/build/AV";
 
 const Dashboard = ({ navigation }) => {
   const bs = useRef();
   const [value, setValue] = useState(false);
-  const [data, setData] = useState(null); //[]
+  const [data, setData] = useState([]); //[]
   const [modalVisible, setModalVisible] = useState(false);
+  const [status, setStatus] = useState("online");
 
   useEffect(async () => {
     try {
       const response = await axios.get("http://10.0.2.2:8000/api/videos/");
       setData(response.data.results);
+      setStatus("online");
     } catch (e) {
-      console.log("error", e);
+      setStatus("offline");
     }
   }, []);
 
@@ -40,100 +43,119 @@ const Dashboard = ({ navigation }) => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.centerScroll}>
-        <ScrollView
-          horizontal={true}
-          alwaysBounceHorizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={styles.ScrollView}
-          contentContainerStyle={{
-            paddingLeft: "30%",
-            paddingRight: "10%",
-            paddingVertical: "20%",
-          }}
-        >
-          <View>
-            <Pressable style={styles.container} onPress={() => setValue(true)}>
-              <View style={styles.buttonContainer}>
-                <View style={styles.button}>
-                  <Image
-                    style={styles.img}
-                    source={require("../../assets/plus.png")}
-                  />
-                </View>
-              </View>
-              <View style={styles.textContainer}>
-                {data === null ? (
-                  <Text style={styles.text}>
-                    Zet jouw eerste video er in om jouw golf process bij te
-                    houden
-                  </Text>
-                ) : (
-                  <Text style={styles.text}>Video uploaden</Text>
-                )}
-              </View>
-            </Pressable>
-          </View>
-          {data === null
-            ? null
-            : data?.map((element) => {
-                return (
-                  <View key={element.id}>
-                    <Pressable
-                      style={{ height: "35%", width: "100%" }}
-                      onPress={() => {
-                        setModalVisible(true);
-                      }}
-                    >
-                      <Card
-                        title={
-                          element.title === undefined
-                            ? "Geen title gekozen"
-                            : element.title
-                        }
-                        date={
-                          element.date === undefined
-                            ? "Geen datum gekozen"
-                            : element.title
-                        }
-                        source={element.video}
-                      />
-                    </Pressable>
-                    <Modal
-                      animationType="slide"
-                      transparent={true}
-                      visible={modalVisible}
-                      onRequestClose={() => {
-                        Alert.alert("Modal has been closed.");
-                        setModalVisible(!modalVisible);
-                      }}
-                    >
-                      <Gallery
-                        video={element.video}
-                        rpm={element.rpm === undefined ? "--" : element.rpm}
-                        heigt={
-                          element.heigt === undefined ? "--" : element.heigt
-                        }
-                        travel={
-                          element.travel === undefined ? "--" : element.travel
-                        }
-                        angle={
-                          element.angle === undefined ? "--" : element.angle
-                        }
-                        xas={element.xas === undefined ? "--" : element.xas}
-                        airtime={
-                          element.airtime === undefined ? "--" : element.airtime
-                        }
-                        model={
-                          element.model === undefined ? simulatie : element.rpm
-                        }
-                      />
-                    </Modal>
+      {status === "offline" ? (
+        <View style={styles.offlineContainer}>
+          <Image
+            style={styles.offlineImg}
+            source={require("../../assets/error.png")}
+          />
+          <Text style={styles.offlineText}>
+            Je bent niet verbonden met het internet
+          </Text>
+        </View>
+      ) : (
+        <View style={styles.centerScroll}>
+          <ScrollView
+            horizontal={true}
+            alwaysBounceHorizontal={true}
+            showsHorizontalScrollIndicator={false}
+            style={styles.ScrollView}
+            contentContainerStyle={{
+              paddingLeft: "30%",
+              paddingRight: "10%",
+              paddingVertical: "20%",
+            }}
+          >
+            <View>
+              <Pressable
+                style={styles.container}
+                onPress={() => setValue(true)}
+              >
+                <View style={styles.buttonContainer}>
+                  <View style={styles.button}>
+                    <Image
+                      style={styles.img}
+                      source={require("../../assets/plus.png")}
+                    />
                   </View>
-                );
-              })}
-        </ScrollView>
-      </View>
+                </View>
+                <View style={styles.textContainer}>
+                  {data.length === 0 ? (
+                    <Text style={styles.text}>
+                      Zet jouw eerste video er in om jouw golf process bij te
+                      houden
+                    </Text>
+                  ) : (
+                    <Text style={styles.text}>Video uploaden</Text>
+                  )}
+                </View>
+              </Pressable>
+            </View>
+            {data.length === 0
+              ? null
+              : data?.map((element) => {
+                  return (
+                    <View key={element.id}>
+                      <Pressable
+                        style={{ height: "35%", width: "100%" }}
+                        onPress={() => {
+                          setModalVisible(true);
+                        }}
+                      >
+                        <Card
+                          title={
+                            element.title === undefined
+                              ? "Geen title gekozen"
+                              : element.title
+                          }
+                          date={
+                            element.date === undefined
+                              ? "Geen datum gekozen"
+                              : element.title
+                          }
+                          source={element.video}
+                        />
+                      </Pressable>
+                      <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                          Alert.alert("Modal has been closed.");
+                          setModalVisible(!modalVisible);
+                        }}
+                      >
+                        <Gallery
+                          video={element.video}
+                          rpm={element.rpm === undefined ? "--" : element.rpm}
+                          heigt={
+                            element.heigt === undefined ? "--" : element.heigt
+                          }
+                          travel={
+                            element.travel === undefined ? "--" : element.travel
+                          }
+                          angle={
+                            element.angle === undefined ? "--" : element.angle
+                          }
+                          xas={element.xas === undefined ? "--" : element.xas}
+                          airtime={
+                            element.airtime === undefined
+                              ? "--"
+                              : element.airtime
+                          }
+                          model={
+                            element.model === undefined
+                              ? simulatie
+                              : element.rpm
+                          }
+                        />
+                      </Modal>
+                    </View>
+                  );
+                })}
+          </ScrollView>
+        </View>
+      )}
       {value && <Popup bs={bs} setValue={setValue} />}
       <NavBar />
     </View>
@@ -187,6 +209,21 @@ const styles = StyleSheet.create({
   img: {
     width: "50%",
     height: "50%",
+  },
+
+  offlineContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  offlineImg: {
+    width: 64,
+    height: 64,
+  },
+
+  offlineText: {
+    fontSize: 15,
   },
 });
 
